e4ce1b5067a1fd53d609a93d6c5300b46aba756a
trystep <alexeevmakc@mail.ru>
2020-07-31 00:28:32 +0300

    fix

--- scraper/scraper_post.py
+++ scraper/scraper_post.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
 import time
+import pyautogui
 
 
 def scraper_post():
@@ -12,16 +14,31 @@ def scraper_post():
         "translate": {"enabled": "True"}
     }
     options.add_experimental_option("prefs", prefs)
-    browser = webdriver.Chrome(options=options, executable_path='/usr/bin/chromedriver')
-    browser.get('https://www.digitalocean.com/blog/how-to-deploy-to-digitalocean-kubernetes-with-github-actions/')
-    time.sleep(10)
+    driver = webdriver.Chrome(options=options, executable_path='/usr/bin/chromedriver')
+    driver.get('https://www.digitalocean.com/blog/how-to-deploy-to-digitalocean-kubernetes-with-github-actions/')
 
-    # Как тут нажать на "Русский" во всплывающем окне гугл-траслейта?
+    # Нажать на "Русский" во всплывающем окне гугл-траслейта
+    time.sleep(1)
+    pyautogui.hotkey("f6")
+    time.sleep(1)
+    pyautogui.press('left')
+    time.sleep(2)
 
-    elem = browser.find_element_by_xpath('//*[@id="gatsby-focus-wrapper"]/div/div[1]/div/div[2]/div/a/h1').text
-    print(elem)
+    # Прокрутка страницы вниз
+    action = ActionChains(driver)
+    for i in range(25):
+        action.send_keys(Keys.DOWN)
+        time.sleep(.5)
+        action.perform()
 
-    browser.quit()
+    time.sleep(1)
+
+    title = driver.find_element_by_xpath('//*[@id="gatsby-focus-wrapper"]/div/div[1]/div/div[2]/div/a/h1').text
+    body = driver.find_element_by_xpath('//*[@id="gatsby-focus-wrapper"]/div/div[1]/div/div[2]/div/div[4]').text
+    print(title)
+    print(body)
+
+    driver.quit()
 
 
 if __name__ == "__main__":
